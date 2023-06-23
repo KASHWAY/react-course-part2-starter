6f013a7b91dcd4a6c10aa6f78bcceca186ec13ad
KASHWAY <targosz.hubert@gmail.com>
2023-06-23 19:35:26 +0200

    Creating filtering with React Query for PostList

--- src/hooks/usepostlist.ts
+++ src/hooks/usepostlist.ts
@@ -8,10 +8,12 @@ interface Post {
     completed: boolean;
 }
 
-const usePosts = () => useQuery<Post[], Error>({
-    queryKey: ['posts'],
+const usePosts = (userId: number | undefined) => useQuery<Post[], Error>({
+    queryKey: userId ? ['users', userId, 'posts'] : ['posts'],
     queryFn: () => axios
-        .get('https://jsonplaceholder.typicode.com/posts')
+        .get('https://jsonplaceholder.typicode.com/posts', {
+            params: { userId }
+        })
         .then(res => res.data),
         staleTime: 1 * 60 * 1000, // 1 minute
 })
