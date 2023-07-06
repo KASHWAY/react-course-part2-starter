d2e016019bee821be24864dc6e01d2a9815aef10
KASHWAY <targosz.hubert@gmail.com>
2023-07-06 18:25:23 +0200

    Exercise- Creating my first own Provider!

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { useReducer } from 'react';
 import './App.css';
 import HomePage from './state-management/HomePage';
 import NavBar from './state-management/NavBar';
-import TasksContext from './state-management/contexts/tasksContext';
-import tasksReducer from './state-management/reducers/tasksReducer';
-import loginReducer from './state-management/reducers/loginReducer';
-import LoginContext from './state-management/contexts/loginContext';
 import LoginProvider from './state-management/LoginProvider';
+import TasksProvider from './state-management/TasksProvider';
 
 function App() {
 
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, loginDispatch] = useReducer(loginReducer, '');
-
   return (
   <>
-    <LoginProvider>
-        <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
-            <NavBar />
-            <HomePage />
-        </TasksContext.Provider>
-    </LoginProvider>
-     
-    
+    <TasksProvider>
+      <LoginProvider>
+          <NavBar />
+          <HomePage />
+      </LoginProvider>
+    </TasksProvider>
   </>
   )
   
